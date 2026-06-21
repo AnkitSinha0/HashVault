adda28bbc92838d903143d0ee7349e96a45c9d66
Ankit Sinha <ankits0057@gmail.com>
2026-06-21 20:52:56 +0530

    feat: added structerefd logger setup (Zap)

---
+++ pkg/logger/logger.go
@@ -0,0 +1,29 @@
+package logger
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var Log *zap.Logger
+
+func Init(env string) {
+	var cfg zap.Config
+
+	if env == "production" {
+		cfg = zap.NewProductionConfig()
+	} else {
+		cfg = zap.NewDevelopmentConfig()
+		cfg.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+	}
+
+	var err error
+	Log, err = cfg.Build()
+	if err != nil {
+		panic("failed to initialise logger: " + err.Error())
+	}
+}
+
+func Sync() {
+	_ = Log.Sync()
+}
