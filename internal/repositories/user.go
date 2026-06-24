664f7170d82b4b83582999fe3db88c19c3d6e12e
Ankit Sinha <ankits0057@gmail.com>
2026-06-24 18:18:20 +0530

    added some more explanation for choice of code logic

--- internal/repositories/user.go
+++ internal/repositories/user.go
@@ -41,6 +41,20 @@ func (r *userRepo) FindByEmail(ctx context.Context, email string) (*models.User,
 //*models.user is a pointer and pointers can be nil so thats why we have 
 // return value as *models.user not models.User cause struct can't be nil 
 // and we chose to have return as nil or the actual user object
+
+// why &user not user because we creatd the user object and we are giving gorm 
+// the address of that object to be filled 
+// var ErrNotFound = errors.New("not found")
+// why ErrNotFound ; cause we dont want to leak gorm specific repositry level error
+// to our service layer 
+// “This was not a normal not-found case. Something else actually went wrong with the DB/query.”
+// So repo just passes the error upward.
+// Problem: if we returned error
+//
+// service now knows about GORM
+// service is coupled to DB library details
+// if you later switch from GORM to raw SQL / sqlx / pgx, service code may need changes
+
 func (r *userRepo) FindByID(ctx context.Context, id uuid.UUID) (*models.User, error) {
 	var user models.User
 	err := r.db.WithContext(ctx).First(&user, "id = ?", id).Error
@@ -49,7 +63,19 @@ func (r *userRepo) FindByID(ctx context.Context, id uuid.UUID) (*models.User, er
 	}
 	return &user, err
 }
+// Model(&models.user{}) 
+// Important: this does not mean we are updating this specific Go object.
+// This empty struct is just used to tell GORM which model/table to target.
+// So it’s basically:
+// use the users table
 
+
+
+// IMPORTANT WHY NOT FETCH AND MODIFY ?
+// BECAUSE 2 DB OPERATION
+// SAFER FOR CONCURRENT OPERATION
+
+// WHY updatecolumn not update cause update will trigger updatedAt etc
 func (r *userRepo) IncrementStorage(ctx context.Context, id uuid.UUID, delta int64) error {
 	return r.db.WithContext(ctx).
 		Model(&models.User{}).
