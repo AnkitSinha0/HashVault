85138588e1cfe479210d5cd30d4cc5c5468134b6
Ankit Sinha <ankits0057@gmail.com>
2026-06-24 21:56:05 +0530

    added somoe comments explaining why uuid is pointer

--- internal/models/folder.go
+++ internal/models/folder.go
@@ -6,7 +6,13 @@ import (
 	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
+// *uuid because folder is optional
+// folder /file somtimes needs to means : there is no parent folder or this file is not
+// inside any folder
 
+// even uuid.Nil means 00000..
+// pointer solves this nil measn in SQL NULL
+// or some value so folder has parent
 type Folder struct {
 	ID             uuid.UUID      `gorm:"type:uuid;primaryKey"                    json:"id"`
 	UserID         uuid.UUID      `gorm:"type:uuid;not null;index:idx_folder_user" json:"user_id"`
